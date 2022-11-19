31477ff87ed2f0f1a34ae324109308371161ce45
Axarios83 <dirk0603@googlemail.com>
2022-11-19 19:04:40 +0100

    Add requirement.txt file

---
+++ server/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/ich", methods=['GET'])
+def render_ich():
+    return render_template("./blogs/ich.html")
+
+@app.route("/gsx600f", methods=['GET'])
+def render_gsx600f():
+    return render_template("./blogs/gsx600f.html")
+
+@app.route("/mt09tracer", methods=['GET'])
+def render_mt09tracer():
+    return render_template("./blogs/mt09tracer.html")
+
+if __name__=='__main__':
+    app.run(debug=True)
